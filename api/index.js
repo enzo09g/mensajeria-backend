e901c43ec36f0fd3d367e300df9cc6467be6470b
Enzo Gómez <enzoeegv@gmail.com>
2024-09-29 11:29:42 -0300

    nuevo intento

--- api/index.js
+++ api/index.js
@@ -8,6 +8,8 @@ app.use(express.json());
 
 app.use(cors({
     origin: 'https://mensajeria-front-end.vercel.app',
+    methods: ['GET', 'POST'],
+    credentials: true
 }));
 
 const register = require('../routes/register')
